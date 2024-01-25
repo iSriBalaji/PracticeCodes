94a3de26b62a02a9a1dfeebde8eee28e643f8970
Sri Balaji Muruganandam <66082342+iSriBalaji@users.noreply.github.com>
2024-01-25 00:27:11 -0500

    24 Jan 2024

---
+++ DailyCode/quicksort.py
@@ -0,0 +1,4 @@
+# to understand the algorithm easily: https://www.youtube.com/watch?v=7h1s2SojIRw
+# quick sort computerphile explanation is good
+# this is based on recurision, divide and conquer
+
